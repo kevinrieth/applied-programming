d8942e74da7ef85eb84ed215be1b042eb59f71ff
kevinrieth <kevin.rieth@kellogg.northwestern.edu>
2020-02-18 02:23:29 +0000

    dice

--- dice.rb
+++ dice.rb
@@ -4,5 +4,9 @@ def view(template); erb template.to_sym; end
 before { puts "Parameters: #{params}" }  
 
 get "/" do
-  "<h1> Hello, world! </h1>"
+    @rand1 = rand(1..6)
+    @rand2 = rand(1..6)
+    view "dice" # embedded Ruby in HTML
+
+  # "<h1>Die 1 = #{rand1}, die 2 = #{rand2}; total = #{total}</h1>"
 end
